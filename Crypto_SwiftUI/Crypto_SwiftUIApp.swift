527ca33280f5e3e0931592de496df23233e37d8f
Raees Nawaj Shaikh <raeesshaikh4222@gmail.com>
2024-02-06 15:15:29 +0530

    Initial Commit

---
+++ Crypto_SwiftUI/Crypto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_SwiftUIApp.swift
+//  Crypto_SwiftUI
+//
+//  Created by Vishal on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
